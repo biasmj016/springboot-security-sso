65643ac2cc504aaac79871d353dfdc3599fbbc30
abcms031 <mjkim031@gmail.com>
2021-06-13 19:13:29 +0900

    first commit

---
+++ src/main/java/com/abcms031/sso/auth/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.abcms031.sso.auth.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+
+    @GetMapping("/welcome")
+    public String print(){
+        return "welcome";
+    }
+}
